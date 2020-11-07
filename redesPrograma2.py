0c408b5d42835441d52996b08b35f8403d56d724
Roniel Nunes <46944438+RonielNunes@users.noreply.github.com>
2020-11-07 14:23:41 -0300

    Add files via upload

--- redesPrograma2.py
+++ redesPrograma2.py
@@ -12,48 +12,50 @@ import random
 
 def slottedAloha(N):
     maquinasRestantes = N #maqinas que falta enviar
-    slots = 500 # pode esprar até mil slots de 51,2u segundos para enviar
-    vetorSlots = []
+    slots = 10 # pode esprar até mil slots de 51,2u segundos para enviar
     timePrimeiroEnvio = 1 #começa com um pois tem colisão no primeiro slots de tempo
     timeEnvioTotalMedio = 1 #começa com um pois tem colisão no primeiro slots de tempo
+    teste = 0
 
     while( maquinasRestantes > 0 ):
 
         #Criando o n slots para cada um enviar
+        print('maquinas restantes:',maquinasRestantes)
+        vetorSlots = []
         for i in range(maquinasRestantes):
             vetorSlots.append(random.randint(1,slots))
 
         #ordena o vetor crescente
         vetorSlots.sort()
-
         print(vetorSlots)
 
         #verifica se tem colisao (tempos iguais)
         if len(set(vetorSlots)) == len(vetorSlots):
-            somaslots = 0
+            if(maquinasRestantes == N):
+                timePrimeiroEnvio = vetorSlots[0]
+            maquinasRestantes = 0
             for i in range(len(vetorSlots)):
                 timeEnvioTotalMedio +=vetorSlots[i] #sem repetição
-            maquinasRestantes = 0
-            timePrimeiroEnvio = vetorSlots[0]
         else:
-            # v = [1,1,1,2,3,4]
             vetorAux = []
             count = 0  #tem maquinas com slots repetidos
             for i in range(len(vetorSlots)):
                 for j in range(len(vetorSlots)):
-                    if (vetorSlots[i] != vetorSlots[j + i]):
-                        vetorAux.append = i #armazena a posição nos indices de slots que n se tem colisão
-                        timeEnvioTotalMedio +=vetorSlots[i] #faz o somatorio do tempo dos slots sem colisão
+                    if ((vetorSlots[i]) == (vetorSlots[j])):
+                        count +=1
 
-
-            if(maquinasRestantes == N): #
+                if (count == 1):
+                    vetorAux.append(i) #armazena a posição nos indices de slots que n se tem colisão
+                    timeEnvioTotalMedio +=vetorSlots[i] #faz o somatorio do tempo dos slots sem colisão
+                count = 0
+            print('posicao a enviar: ',vetorAux)
+            if(maquinasRestantes == N and len(vetorAux) != 0):
                 for i in range(len(vetorSlots)):
                     if(vetorAux[0] == i):
-                        print(vetorSlots)
                         timePrimeiroEnvio += vetorSlots[i]
 
-            else:
-                maquinasRestantes = N - len(vetorAux)
+            maquinasRestantes = maquinasRestantes - len(vetorAux)
+
 
     vetorResultado = [timePrimeiroEnvio*51.2,timeEnvioTotalMedio*51.2]
     return vetorResultado
@@ -61,5 +63,9 @@ def slottedAloha(N):
 
 
 if __name__ == '__main__':
-    N = 20 #Numero de estamos que vamo tentar transmitir
-    print(slottedAloha(N))
+    N = 10 #Numero de estamos que vamo tentar transmitir
+    vertorResultado = slottedAloha(N)
+    print('**********Saida**********')
+    print(vertorResultado)
+    print('Tempo primeiro envio: ',vertorResultado[0],'microssegundos')
+    print('Tempo medio total: ',(vertorResultado[1])/(N),'microssegundos')
