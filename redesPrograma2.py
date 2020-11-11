f78d78b37ea96dbdfc3f11ef3de76727bb6d329a
Roniel Nunes <46944438+RonielNunes@users.noreply.github.com>
2020-11-11 14:58:45 -0300

    Add files via upload
    Criação do csma e backoff

--- redesPrograma2.py
+++ redesPrograma2.py
@@ -1,4 +1,5 @@
 import random
+import statistics
 
 #primeiro todas as maquinas vao tentar comunicar
 #vamos ter colisão
@@ -12,7 +13,7 @@ import random
 
 def slottedAloha(N):
     maquinasRestantes = N #maqinas que falta enviar
-    slots = 10 # pode esprar até mil slots de 51,2u segundos para enviar
+    slots = 100 # pode esprar até mil slots de 51,2u segundos para enviar
     timePrimeiroEnvio = 1 #começa com um pois tem colisão no primeiro slots de tempo
     timeEnvioTotalMedio = 1 #começa com um pois tem colisão no primeiro slots de tempo
     teste = 0
@@ -20,14 +21,14 @@ def slottedAloha(N):
     while( maquinasRestantes > 0 ):
 
         #Criando o n slots para cada um enviar
-        print('maquinas restantes:',maquinasRestantes)
+        #print('maquinas restantes:',maquinasRestantes)
         vetorSlots = []
         for i in range(maquinasRestantes):
             vetorSlots.append(random.randint(1,slots))
 
         #ordena o vetor crescente
         vetorSlots.sort()
-        print(vetorSlots)
+        #print(vetorSlots)
 
         #verifica se tem colisao (tempos iguais)
         if len(set(vetorSlots)) == len(vetorSlots):
@@ -48,7 +49,7 @@ def slottedAloha(N):
                     vetorAux.append(i) #armazena a posição nos indices de slots que n se tem colisão
                     timeEnvioTotalMedio +=vetorSlots[i] #faz o somatorio do tempo dos slots sem colisão
                 count = 0
-            print('posicao a enviar: ',vetorAux)
+            #print('posicao a enviar: ',vetorAux)
             if(maquinasRestantes == N and len(vetorAux) != 0):
                 for i in range(len(vetorSlots)):
                     if(vetorAux[0] == i):
@@ -57,15 +58,153 @@ def slottedAloha(N):
             maquinasRestantes = maquinasRestantes - len(vetorAux)
 
 
+
     vetorResultado = [timePrimeiroEnvio*51.2,timeEnvioTotalMedio*51.2]
+    print('aloha')
     return vetorResultado
 
+def csma(N):
+    #Considerando que houve colisão, o csma calculará um tempo de espera para todas as maquinas.
+    totalElementos = N
+    slots = 100
+    timeEnvioTotalMedio = 0
+    timePrimeiroEnvio = 0
+    vetorResultado = []
+
+    while (totalElementos > 0):
+        vetorSlots = []
+        for i in range(totalElementos):
+            vetorSlots.append(random.randint(1,slots))
+        vetorSlots.sort()
+        #print(vetorSlots)
+        #verifica se o canal tá ocupado
+        #caso que n tá ocupado
+        if len(set(vetorSlots)) == len(vetorSlots):
+            for i in range(len(vetorSlots)):
+                timeEnvioTotalMedio +=vetorSlots[i]
+            if (len(vetorSlots) == N):
+                timePrimeiroEnvio = vetorSlots[0]
+                break
+            totalElementos = 0
+        else:#ta ocupado
+            #Quais que estão ocupados
+            vetorPosica = []
+            for i in range(totalElementos):
+                count = 0
+                for j in range(totalElementos):
+                    if (vetorSlots[i] == vetorSlots[j]):
+                        count +=1
+                if count > 1:
+                    vetorPosica.append(i) #guarda a posicao dos elementos que devem ser sorteados novamnete
+                else:
+                    if (len(vetorSlots) == N):
+                        timePrimeiroEnvio = vetorSlots[0]
+                    timeEnvioTotalMedio +=vetorSlots[i]
+
+
+            totalElementos -= (len(vetorSlots) - len(vetorPosica))
+            #passa que tem tempo diferente
+
+    #print('valor:',totalElementos)
+
+    vetorResultado.append(timePrimeiroEnvio*51.2)
+    vetorResultado.append(timeEnvioTotalMedio*51.2)
+    print('csma')
+    return vetorResultado
+
+def backoff(N): #limite de tentativas
+    i = 1 #formula (0,2^i - 1)
+    timeEnvioTotalMedio = 0
+    timePrimeiroEnvio = 0
+    estacoes = N
+    slots = 100
+    vetorResultado = []
+    while(estacoes > 0 or i < 16):
+        vetorSlots = []
+
+        for i in range(estacoes):
+            vetorSlots.append(random.randint(0,slots))
+        vetorSlots.sort()
+        #print(vetorSlots)
+        #verificando se tivemos colisao entre as estações
+        if len(set(vetorSlots)) == len(vetorSlots):
+            for i in range(len(vetorSlots)):
+                timeEnvioTotalMedio +=vetorSlots[i]
+            if (len(vetorSlots) == N):
+                timePrimeiroEnvio = vetorSlots[0]
+                break
+            estacoes = 0
+        else:
+            #tivemos colisao
+            vetorPosica = []
+
+            #indentifica quias estações se colidiram
+            for i in range(estacoes):
+                count = 0
+                for j in range(estacoes):
+                    if (vetorSlots[i] == vetorSlots[j]):
+                        count +=1
+                if count > 1:
+                    vetorPosica.append(i) #guarda a posição de colisão
+                else:
+                    if (len(vetorSlots) == N):
+                        timePrimeiroEnvio = vetorSlots[0] #guarda o primeiro tempo da estação que enviou
+                    timeEnvioTotalMedio +=vetorSlots[i]   #guarda o tempo das estações que eviamos
+
+                estacoes -= (len(vetorSlots) - len(vetorPosica))
+                print('valor estacao:')
+                slots = (2**i) - 1
+                i +=1
+                print('valor i:',i )
+
+    if (i == 16):
+        print("tempo maximo atigindo")
+        return 0
+    else:
+        vetorResultado.append(timePrimeiroEnvio*51.2)
+        vetorResultado.append(timeEnvioTotalMedio*51.2)
+        print('backoff')
+        return  vetorResultado
 
 
 if __name__ == '__main__':
     N = 10 #Numero de estamos que vamo tentar transmitir
-    vertorResultado = slottedAloha(N)
-    print('**********Saida**********')
-    print(vertorResultado)
-    print('Tempo primeiro envio: ',vertorResultado[0],'microssegundos')
-    print('Tempo medio total: ',(vertorResultado[1])/(N),'microssegundos')
+    print("Numero de estacoes(N):",N)
+
+
+    vetorAlohaPrimeiro = []
+    vetorAlohaTotal = []
+
+    vetorCsmaPrimeiro = []
+    vetorCsmaTotal = []
+
+    vetorBackoffPrimeiro = []
+    vetorBackoffTotal = []
+
+    for i in range(33):
+        print(i)
+        vetorAlohaResultado = slottedAloha(N)
+        vetorAlohaPrimeiro.append(vetorAlohaResultado[0])
+        vetorAlohaTotal.append(vetorAlohaResultado[1])
+
+        vetorCsmaResultado = csma(N)
+        vetorCsmaPrimeiro.append(vetorCsmaResultado[0])
+        vetorCsmaTotal.append(vetorCsmaResultado[1])
+
+        vetorBackoffResultado = backoff(N)
+        vetorBackoffPrimeiro.append(vetorBackoffResultado[0])
+        vetorBackoffTotal.append(vetorBackoffResultado[1])
+
+
+    print("ALOHA")
+    print(statistics.mean(vetorAlohaPrimeiro))
+    print(statistics.mean(vetorAlohaTotal))
+
+
+    print("CSMA")
+    print(statistics.mean(vetorCsmaPrimeiro))
+    print(statistics.mean(vetorCsmaTotal))
+
+    print("BACKOFF")
+    print(statistics.mean(vetorBackoffPrimeiro))
+    print(statistics.mean(vetorBackoffTotal))
